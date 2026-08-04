43b6d0e3ff7d4473aa54a08767de18afd580be29
Dilshit <dilshikk@mail.ru>
2026-08-05 04:43:30 +0500

    feat: register InterviewSession in models __init__

--- bot/db/models/__init__.py
+++ bot/db/models/__init__.py
@@ -1,8 +1,9 @@
-# bot/db/models package
+# bot/db/models/__init__.py
 
 from bot.db.models.application import Application
 from bot.db.models.blacklist import Blacklist
+from bot.db.models.interview import InterviewSession
 from bot.db.models.user import User
 from bot.db.models.vacancy import Vacancy
 
-__all__ = ["Application", "Blacklist", "User", "Vacancy"]
+__all__ = ["Application", "Blacklist", "InterviewSession", "User", "Vacancy"]
