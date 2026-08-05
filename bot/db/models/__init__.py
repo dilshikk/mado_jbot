2b0474793d0f5e4af2a246d5bb8de24ef95e89e1
Dilshit <dilshikk@mail.ru>
2026-08-05 13:59:58 +0500

    feat: зарегистрировать MetroStation в моделях

--- bot/db/models/__init__.py
+++ bot/db/models/__init__.py
@@ -3,7 +3,8 @@
 from bot.db.models.application import Application
 from bot.db.models.blacklist import Blacklist
 from bot.db.models.interview import InterviewSession
+from bot.db.models.metro_station import MetroStation
 from bot.db.models.user import User
 from bot.db.models.vacancy import Vacancy
 
-__all__ = ["Application", "Blacklist", "InterviewSession", "User", "Vacancy"]
+__all__ = ["Application", "Blacklist", "InterviewSession", "MetroStation", "User", "Vacancy"]
